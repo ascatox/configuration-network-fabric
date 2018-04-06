57c3ec37fdf40c4fad87a666154f3f7ded4bba63
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-04-06 11:26:14 +0200

    New script_reduced for restart

--- initNetwork.sh
+++ initNetwork.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "WARNING RESET ALL DATA of HLF"
 echo "REBUILD Network!!!!"
-./byfn.sh -m reset -c ledgerchannel -s couchdb
+sudo rm -rf ./data/* && ./byfn.sh -m reset -c ledgerchannel -s couchdb
