e6255e5b88b7f9786ad42e3decffa14deec31c68
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-07-25 15:44:06 +0200

    Update createDestination.sh

--- createDestination.sh
+++ createDestination.sh
@@ -5,14 +5,10 @@ DESTINATION="crypto-config-output"
 
 if [ "$1" == "" ]; then
 	echo "Insert the name of the user to create'"
-	echo "Usage ./createIdentity <name> <destination folder>"
+	echo "Usage ./createIdentity <name>"
 	exit 0
-fi
-
-if [ "$2" != "" ]; then
-	DESTINATION = $2
 fi
 
 mkdir -p $DESTINATION/crypto-config/peerOrganizations/$ORG_NAME/users
 cp -fR ./crypto-config/peerOrganizations/org1.example.com/users/$1@$ORG_NAME $DESTINATION/crypto-config/peerOrganizations/org1.example.com/users
-tar -zcvf crypto-config-$1.tar.gz $DESTINATION && rm -rf $DESTINATION
+tar -zcvf crypto-config-$1.tar.gz ./$DESTINATION/crypto-config && rm -rf $DESTINATION
