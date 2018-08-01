d9b8c85d8a7509b6750bbffa64f2284ed0e77b35
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-08-01 15:19:56 +0200

    Update createIdentity.sh

--- createIdentity.sh
+++ createIdentity.sh
@@ -2,23 +2,39 @@
 
 ORG_NAME="org1.example.com"
 TYPE="user"
+PASSWORD="faredge2018"
+#ATTRS='"hf.Registrar.Roles=peer,client,user"'
+UUID=$(cat /proc/sys/kernel/random/uuid)
+ATTRS="role=dcot-operator:ecert,uid=$UUID:ecert"
+
 
 if [ "$1" == "" ]; then
 	echo "Insert the name of the user to create'"
-	echo "Usage ./createIdentity <name> <user/peer>"
+	echo "Usage ./createIdentity <name> <password> <user/peer>"
 	exit 0
 fi
 
 if [ "$2" != "" ]; then
-	TYPE = $2
+	PASSWORD=$2
 fi
 
+if [ "$3" != "" ]; then
+	TYPE=$3
+fi
+
+# id.maxenrollments -1 infinite
 docker exec -it ca.example.com fabric-ca-client enroll -u http://admin:adminpw@localhost:7054
 
-docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.attrs "dcot-operator=true" --id.type $TYPE  --id.secret faredge2018
-docker exec -it ca.example.com  fabric-ca-client enroll -u http://$1:faredge2018@ca.example.com:7054 --enrollment.attrs "dcot-operator"
+docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.type $TYPE  --id.secret $PASSWORD --id.maxenrollments -1 --id.attrs $ATTRS
+docker exec -it ca.example.com  fabric-ca-client enroll -u http://$1:faredge2018@ca.example.com:7054 -M crypto-config/peerOrganizations/org1.example.com/msp
+echo "UID is $UUID"
+# Stop fabric-ca-server.
+# Copy crypto-config/peerOrganizations/<orgName>/ca/*pem to $FABRIC_CA_SERVER_HOME/ca-cert.pem.
+# Copy crypto-config/peerOrganizations/<orgName>/ca/*_sk to $FABRIC_CA_SERVER_HOME/msp/keystore/.
+# Start fabric-ca-server.
+# Delete any previously issued enrollment certificates and get new certificates by enrolling again.
 
-mkdir -p ./crypto-config/peerOrganizations/$ORG_NAME/users/$1@$ORG_NAME/msp/signcerts
-mkdir -p ./crypto-config/peerOrganizations/$ORG_NAME/users/$1@$ORG_NAME/msp/keystore
-cp ./ca/data/msp/signcerts/cert.pem ./crypto-config/peerOrganizations/$ORG_NAME/users/$1@$ORG_NAME/msp/signcerts/$1@$ORG_NAME-cert.pem
-cd ./ca/data/msp/keystore && cp $(ls -t | head -1) ../../../../crypto-config/peerOrganizations/$ORG_NAME/users/$1@$ORG_NAME/msp/keystore
+docker stop ca.example.com
+sudo cp crypto-config/peerOrganizations/$ORG_NAME/ca/*pem ca/data/ca-cert.pem
+sudo cp crypto-config/peerOrganizations/$ORG_NAME/ca/*_sk ca/data/msp/keystore/.
+docker start ca.example.com
