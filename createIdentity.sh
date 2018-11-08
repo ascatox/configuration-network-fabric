e5bea1ec4c3f84a0189bca6302562ae963d3c48f
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-11-08 12:36:03 +0100

    Update createIdentity.sh

--- createIdentity.sh
+++ createIdentity.sh
@@ -2,7 +2,7 @@
 
 ORG_NAME="org1.example.com"
 TYPE="user"
-PASSWORD="faredge2018"
+#PASSWORD="faredge2018"
 #ATTRS='"hf.Registrar.Roles=peer,client,user"'
 #UUID=$(cat /proc/sys/kernel/random/uuid)
 #DCOT_ROLE="dcot-user"
@@ -18,7 +18,6 @@ if [ "$2" == "" ]; then
 	echo "Insert the 'password' of the user to create"
 	echo "Usage ./createIdentity <name> <password> <user/peer>"
 	exit 0
-	PASSWORD=$2
 fi
 
 if [ "$3" != "" ]; then
@@ -31,9 +30,9 @@ fi
 # id.maxenrollments -1 infinite
 docker exec -it ca.example.com fabric-ca-client enroll -u http://admin:adminpw@localhost:7054
 
-docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.type $TYPE  --id.secret $PASSWORD --id.maxenrollments -1 
+docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.type $TYPE  --id.secret $2 --id.maxenrollments -1 
 #--id.attrs $ATTRS
-docker exec -it ca.example.com  fabric-ca-client enroll -u http://$1:$PASSWORD@ca.example.com:7054 -M crypto-config/peerOrganizations/org1.example.com/msp
+docker exec -it ca.example.com  fabric-ca-client enroll -u http://$1:$2@ca.example.com:7054 -M crypto-config/peerOrganizations/org1.example.com/msp
 echo "UID is $UUID"
 echo "UID of $1 is $UUID with role $DCOT_ROLE" >> log_uids.txt
 
