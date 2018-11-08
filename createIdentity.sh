e589e3bbb13a75cd8ba4055737f86ef0dfc86c03
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-11-08 12:33:28 +0100

    New script createIdentity.sh

--- createIdentity.sh
+++ createIdentity.sh
@@ -4,37 +4,35 @@ ORG_NAME="org1.example.com"
 TYPE="user"
 PASSWORD="faredge2018"
 #ATTRS='"hf.Registrar.Roles=peer,client,user"'
-UUID=$(cat /proc/sys/kernel/random/uuid)
-DCOT_ROLE="dcot-user"
+#UUID=$(cat /proc/sys/kernel/random/uuid)
+#DCOT_ROLE="dcot-user"
 BASEDIR=$(pwd)
 
 if [ "$1" == "" ]; then
 	echo "Insert the 'name' of the user to create"
-	echo "Usage ./createIdentity <name> <dcot-user/dcot-operator> <password> <user/peer> <uid>"
+	echo "Usage ./createIdentity <name> <password> <user/peer>"
 	exit 0
 fi
 
-if [ "$2" != "" ]; then
-	DCOT_ROLE=$2
+if [ "$2" == "" ]; then
+	echo "Insert the 'password' of the user to create"
+	echo "Usage ./createIdentity <name> <password> <user/peer>"
+	exit 0
+	PASSWORD=$2
 fi
 
 if [ "$3" != "" ]; then
-	PASSWORD=$3
+	TYPE=$3
 fi
 
-if [ "$4" != "" ]; then
-	TYPE=$4
-fi
 
-if [ "$5" != "" ]; then
-	UUID=$5
-fi
-ATTRS="role=$DCOT_ROLE:ecert,uid=$UUID:ecert"
+#ATTRS="role=$DCOT_ROLE:ecert,uid=$UUID:ecert"
 
 # id.maxenrollments -1 infinite
 docker exec -it ca.example.com fabric-ca-client enroll -u http://admin:adminpw@localhost:7054
 
-docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.type $TYPE  --id.secret $PASSWORD --id.maxenrollments -1 --id.attrs $ATTRS
+docker exec -it ca.example.com fabric-ca-client register  --id.name $1  --id.type $TYPE  --id.secret $PASSWORD --id.maxenrollments -1 
+#--id.attrs $ATTRS
 docker exec -it ca.example.com  fabric-ca-client enroll -u http://$1:$PASSWORD@ca.example.com:7054 -M crypto-config/peerOrganizations/org1.example.com/msp
 echo "UID is $UUID"
 echo "UID of $1 is $UUID with role $DCOT_ROLE" >> log_uids.txt
